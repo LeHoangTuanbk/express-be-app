fd0fc96887f2d9df9c684d3466864f9f6b816e98
Thanh Nguyen Ngoc <thanhthanchuc@gmail.com>
2020-06-04 11:58:20 +0700

    feat: update starting flow

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+var path = require('path');
 
 const person = require('./apis/person')
 
@@ -11,5 +12,9 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.json());
 
 app.use('/api/person', person)
+
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/pages/index.html'))
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
