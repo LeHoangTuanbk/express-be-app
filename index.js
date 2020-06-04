d2b2bbcef3fb82d59588c2cfaba6d32163aec945
Thanh Nguyen Ngoc <thanhthanchuc@gmail.com>
2020-06-04 12:26:48 +0700

    feat: add login view

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
 
 app.use('/api/person', person)
 
 app.use('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/pages/index.html'))
+  res.sendFile(path.join(__dirname + '/public/pages/index.html'))
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
